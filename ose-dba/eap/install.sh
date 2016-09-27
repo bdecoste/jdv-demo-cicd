5cf6581738245dccd12d6ebafbcc395973a42a6e
Ramesh Reddy <rareddy@jboss.org>
2016-09-27 14:56:56 -0500

    Update install.sh

--- ose-dba/eap/install.sh
+++ ose-dba/eap/install.sh
@@ -6,6 +6,8 @@ source /usr/local/s2i/install-common.sh
 
 injected_dir=$1
 
+install_deployments ${injected_dir}/injected-deployments
+
 install_modules ${injected_dir}/injected-modules
 
 configure_drivers ${injected_dir}/install.properties
